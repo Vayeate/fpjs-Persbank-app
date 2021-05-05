5f52e1ef2d109a10de0b7f4e3271b1aee59a0b87
Vayeate <vayeate@europe.com>
2021-05-05 22:28:07 +0300

    Edited the index.html, added style.css, added script.js

---
+++ jsver/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+///////PersBank
+
+// Data
+const account1 = {
+  owner: "Aleksei Uljassov",
+  movements: [50, 450, -250, 1500, -350, -130, -70, 800],
+  interestRate: 1.2, // %
+  pin: 1111
+};
+
+const account2 = {
+  owner: "Kaire Krants",
+  movements: [2500, 700, -150, -790, -3210, -1000, 8500, -300],
+  interestRate: 1.5,
+  pin: 2222
+};
+
+const accounts = [account1, account2];
