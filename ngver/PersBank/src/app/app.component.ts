4d02b69d038529cfab6cb312f883c877282f7ff5
Vayeate <vayeate@europe.com>
2021-05-08 22:21:12 +0300

    Edited the CSS.

--- ngver/PersBank/src/app/app.component.ts
+++ ngver/PersBank/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PersBank';
+  account1 = {
+    owner: 'Aleksei Uljassov',
+    movements: [50, 450, -250, 1500, -350, -130, -70, 800],
+    interestRate: 1.2, // %
+    pin: 1111
+  };
+
+  account2 = {
+    owner: 'Kaire Krants',
+    movements: [2500, 700, -150, -790, -3210, -1000, 8500, -300],
+    interestRate: 1.5,
+    pin: 2222
+  };
 }
