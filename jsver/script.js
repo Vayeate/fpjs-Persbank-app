a60358e5b640be5edfbc02e710e9046b274a15af
Vayeate <vayeate@europe.com>
2021-05-16 12:14:59 +0300

    Edited the css , implemented the current date.

--- jsver/script.js
+++ jsver/script.js
@@ -38,6 +38,7 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 const btnLoan = document.querySelector(".form__btn--loan");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const labelDate = document.querySelector(".date");
 
 const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = "";
@@ -195,3 +196,11 @@ btnSort.addEventListener("click", function(e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = `${now.getHours()}`.padStart(2, 0);
+const min = `${now.getMinutes()}`.padStart(2, 0);
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
